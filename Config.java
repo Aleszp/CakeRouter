cd6d04a3e4bbcd83f2a6405dddfcb3395b9bfc79
aleszp <aleksander.szsz@gmail.com>
2016-06-15 18:03:53 +0200

    Minimalna zmiana testowego IP (testy w obrębie jednego komputera, więc zamiast swojego IP ustawiłem loopback

--- Config.java
+++ Config.java
@@ -7,7 +7,7 @@ public class Config
 
     public static final int BUFFER_SIZE = 512;
     //adresy węzłów i serwera
-    public static final int[] IP1={192,168,1,2};
-    public static final int[] IP2={192,168,1,2};
-    public static final int[] IPFin={192,168,1,2};
+    public static final int[] IP1={127,0,0,1};
+    public static final int[] IP2={127,0,0,1};
+   
 }
